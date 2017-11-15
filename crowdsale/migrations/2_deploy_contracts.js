2753a7c344c6da4be1f7cd11a1ebb49e923c0aaf
Zoltan <mzol14@gmail.com>
2017-11-15 20:37:13 +0000

    finalized crowsale contract for live - except for gas

--- crowdsale/migrations/2_deploy_contracts.js
+++ crowdsale/migrations/2_deploy_contracts.js
@@ -1,28 +1,13 @@
 var ByteBetCrowdsale = artifacts.require("ByteBetCrowdsale");
 
 module.exports = function(deployer, network, accounts) {
-    const now = Math.floor((new Date()).getTime() / 1000);
-    
-    const seconds = 1;
-    const minutes = 60 * seconds;
-    const hours = 60 * minutes;
-    const days = 24 * hours;
-    const weeks = 7 * days;
- 
-    const startTime = web3.eth.getBlock('latest').timestamp + 15 * minutes;
-    const endTime = startTime + (15 * minutes);
+    const startTime = 1511287200; // Tuesday, 21 November 2017 18:00:00
+    const endTime = 1511978400; // Wednesday, 29 November 2017 18:00:00
     const rate = 130;
     const goal = web3.toWei(150, "ether");
     const cap = web3.toWei(600, "ether");
-    const wallet = "0xab379b2255b66c2829ed74b7e7d6a51e090904f8"
-
-    console.log("startTime", startTime);
-    console.log("endTime", endTime);
-    console.log("rate", rate);
-    console.log("goal", goal);
-    console.log("cap", cap);
-    console.log("wallet", wallet);
+    const wallet = "0x7FDEDa642F9aD839a024612EA1c7B8860D609d71"
 
     // uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _goal, uint256 _cap, address _wallet
-    deployer.deploy(ByteBetCrowdsale, startTime, endTime, rate, goal, cap, wallet, {gas: 4500000})
+    deployer.deploy(ByteBetCrowdsale, startTime, endTime, rate, goal, cap, wallet)
 };
--- crowdsale/truffle.js
+++ crowdsale/truffle.js
@@ -13,9 +13,16 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
-      gas: 5000000,
+      gas: 500000,
       from: "0x49fdcd92118bfef5c0517d31d24259bfe9854b8d",
     },
+    live: {
+      network_id: 1,
+      host: "127.0.0.1",
+      port: 8546,
+      from: "0x006e5b6b52a1d85fe16e3b040d211bc23ed98f8a",
+      gas: 2200000,
+    }
   },
 	solc: {
 		optimizer: {
